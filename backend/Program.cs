8af651c69d63205d730c90555fd1bba04514833f
AyalaLoifer <5808004@gmail.com>
2025-12-20 18:52:58 +0200

    update cors

--- backend/Program.cs
+++ backend/Program.cs
@@ -15,16 +15,12 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend",
-        policy => policy
-            .WithOrigins(
-                "http://localhost:3000",
-                "https://todolist-frontend-km9z.onrender.com"
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials() 
-    );
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
 });
 
 
